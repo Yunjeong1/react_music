4d0f40f81802fb60445e6cfe194d71a2688e43ba
tubi55 <tubi55@nate.com>
2022-04-06 16:19:44 +0900

    구조분해할당으로 article컴포넌트의 props로 전달받기

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Article from './Article';
+
+//hook : 컴포넌트 안쪽에 다양한 기능을 도와주는 모듈
+//useState: 컴포넌트의 랜더링에 관여하는 중요한 정보값을 관리해주는 훅
+
+/*
+구조분해 할당
+비구조화 할당
+destructuring assignment;
+*/
+//각 배열의 데이터를 구조분해 할당을 이용해서 원하는 변수명으로 한번에 할당
+const fruit = ['apple', 'strawberry', 'melon'];
+const [a, b, c] = fruit;
+
+const student = {
+	name: '홍길동',
+	age: 20,
+	isFemale: false,
+};
+
+const { name } = student;
+console.log(name);
 
 function Section() {
 	const arr = [
@@ -15,8 +36,7 @@ function Section() {
 	return (
 		<section>
 			{arr.map((data, idx) => {
-				//부모요소에서 자식 컴포넌트로 prop을 통해서 특정 데이터 전달 가능
-				return <Article key={idx} data={data} />;
+				return <Article key={idx} data={data} index={idx} />;
 			})}
 		</section>
 	);
