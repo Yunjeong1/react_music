1ee5625a3f3d525a5ab117de88eb63026f5d9607
tubi55 <tubi55@nate.com>
2022-04-06 15:41:27 +0900

    public폴더로 이미지 배치해서 public폴더까지의 절대경로로 이미지소스 url연결

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+//public폴더까지의 절대 경로값
+const path = process.env.PUBLIC_URL;
+
 function Article(props) {
 	console.log(props);
 	return (
 		<article>
 			<h2>{props.data}</h2>
-			<img src={props.imgSrc} />
+			{/* public폴더까지의 절대경로를 불러와서 그 뒤에 props로 전달받은 이미지명을 이용해 이미지소스 url완성 */}
+			<img src={`${path}/img/${props.data}.jpg`} />
 		</article>
 	);
 }
